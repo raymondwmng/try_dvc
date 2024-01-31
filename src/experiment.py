d962e47b2d6237177e83d1da8908cb5c67971324
Raymond Ng <cuhk.raymond@gmail.com>
2024-01-31 12:32:15 +0000

    add experiment to dvc

--- src/experiment.py
+++ src/experiment.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 import sys
+import yaml
 
 def try_experiment(expt_name=""):
     expt_time = datetime.now()
-    print("I am trying experiment {expt_name}")
+    print(f"I am trying experiment {expt_name}")
     print(f"Time now is {expt_time}")
 
     with open("./data/output.log","w") as fo:
@@ -11,5 +12,8 @@ def try_experiment(expt_name=""):
     
 
 if __name__=='__main__':
-    expt_name=sys.argv[1]
+    # expt_name=sys.argv[1]
+    with open('./params.yaml') as f:
+        params=yaml.safe_load(f)
+    expt_name=params['experiment']['name']
     try_experiment(expt_name)
